b3d9f873dbbbb62840d10a814bb5674853f8eee7
shunich <fukusyun3@gmail.com>
2023-02-01 22:25:57 +0900

    feat: env利用

--- src/database/db.go
+++ src/database/db.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"ambassador/src/models"
+	"fmt"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,8 +13,13 @@ var DB *gorm.DB
 
 func Connect() {
 	var err error
+	user := os.Getenv("MySQL_USER")
+	password := os.Getenv("MYSQL_PASSWORD")
+	host := os.Getenv("MYSQL_HOST")
+	dbname := os.Getenv("MYSQL_DATABASE")
+	connection := fmt.Sprintf("%s:%s@tcp(%s)/%s", user, password, host, dbname)
 
-	DB, err = gorm.Open(mysql.Open("root:root@tcp(ambassador_mysql)/ambassador"), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(connection), &gorm.Config{})
 
 	if err != nil {
 		panic("データベースの接続に失敗しました!")
